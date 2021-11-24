4d0560a111b5f6991a92a4c9c0ea5623b842072d
veronicaferreyra5 <veronicaferreyra5@gmail.com>
2021-11-23 21:21:44 -0300

    feat: Add blabla

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET nosotros page */
+router.get('/nosotros', (req, res) => {
+  res.render('pages/nosotros', { title: 'Nosotros' });
+});
+
+/* GET contacto page */
+router.get('/contacto', (req, res) => {
+  res.render('pages/contacto', { title: 'Contacto' });
+});
+
 module.exports = router;
